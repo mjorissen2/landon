367378fe90952a76ce2b2acad07130bca6d344fb
Matthijs Jorissen <jorissen2@gmail.com>
2024-03-01 14:28:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React in less then  a Day
+          Learn React in less then  a week
         </a>
       </header>
     </div>
